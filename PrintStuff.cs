a58e1cd6afc8c97991c890c0d3e8742af519be53
= <zm275@byu.edu>
2023-01-18 13:05:39 -0700

    added comments

--- PrintStuff.cs
+++ PrintStuff.cs
@@ -12,7 +12,10 @@ namespace CSharpFun
         {
             /*create an int array of length 12 */
             int[] rollValues = new int[12];
+            
+            /*Create a object type random*/
             var random = new Random();
+
             /*create a for loop that goes through all throws*/
             for (int i = 0; i < numThrows; i++)
             {
@@ -20,6 +23,7 @@ namespace CSharpFun
                 int roll1 = random.Next(1, 7);
                 int roll2 = random.Next(1, 7);
                 int total = roll1 + roll2;
+
                 /*increment the int in the array according to the value of the dice rolls*/
                 rollValues[total - 1]++;
               
@@ -32,7 +36,11 @@ namespace CSharpFun
                 double rollTotal = rollValues[i];
                 percent = (rollTotal / numThrows)* 100;
                 percent = Math.Round(percent, 0, MidpointRounding.AwayFromZero);
+                
+                /*Reset the astrics string*/
                 astrics = "";
+
+                /*Print an astric for each percent of the throws for each number combination*/
                 for (int j = 1; j < percent + 1; j++)
                 {
                     astrics = astrics + '*';
--- Program.cs
+++ Program.cs
@@ -10,25 +10,15 @@ namespace CSharpFun
     {
         public static void Main(string[] args)
         {
-            /* PrintStuff ps;
-             PrintStuff ps2 = new PrintStuff();
-             string userName = "";
-
-             Console.Write("Please enter a name: ");
-             userName = Console.ReadLine();
-             ps = new PrintStuff();
-             ps.PrintName(userName);
-             Console.Write("Please enter a name: ");
-             string tacobell = "";
-             tacobell = Console.ReadLine();*/
+          
             Console.WriteLine("Welcome to the dice throwing simulator!");
             Console.WriteLine("How many dice rolls would you like to simulate? ");
             int numThrows = 0;
             numThrows = int.Parse(Console.ReadLine());
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS \n Each * represents 1 % of the total number of rolls. \nTotal number of rolls = {0}.", numThrows);
-
+/*create a new dice instance*/
             var dice = new Dice();
-
+/*calculate the throws and print the histogram*/
             dice.CalculateThrows(numThrows);
 
 
